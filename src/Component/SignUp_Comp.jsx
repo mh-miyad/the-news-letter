96738b51f410cc3ff9599677173dce41188aa3f8
miyad <mhmiyad6565@gmail.com>
2023-04-25 10:27:45 +0600

    set log in page

--- src/Component/SignUp_Comp.jsx
+++ src/Component/SignUp_Comp.jsx
@@ -50,6 +50,10 @@ const SignUp_Comp = () => {
       >
         Sign Up
       </Button>
+      <div className='flex justify-center gap-10'>
+    <button className='text-white bg-gradient-to-tr from-emerald-500 from-25% hover:from-30%  hover:via-40% to-lime-600  via-green-400 via-30% to-70% hover:to-60%  px-10 py-2 rounded  hover:px-8 hover:py-1 transition-all duration-100 capitalize font-semibold shadow-2xl shadow-red-900'>google</button>
+    <button className='text-white bg-gradient-to-tr from-blue-500 from-25% hover:from-30%  hover:via-40% to-sky-600  via-cyan-400 via-30% to-70% hover:to-60%  px-10 py-2 rounded  hover:px-8 hover:py-1 transition-all duration-100 capitalize font-semibold shadow-2xl shadow-blue-900'>Facebook</button>
+  </div>
     </form></div>
     </div>
   );
