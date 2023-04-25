81a4a2773d8604251d9ffb299504362d30e3ec8d
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-04-25 19:21:13 +0600

    yml file

--- src/Provider/Authprovider.jsx
+++ src/Provider/Authprovider.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createContext } from 'react'
+import app from '../Firebase/Firebase.init'
  export const AuthContext = createContext(null);
+ import getAuth from 'firebase'
 
 
- const user =  (true)
+
+   const auth = getAuth(app)
 
 const Authprovider = ({children}) => {
+
+  const [ user , setUser ] = useState(null);
+  const    userLoger = ()=>{
+    return    create
+  }
+
+ const  authProvider = {
+  user ,
+  userLoger,
+
+
+ }
+
   return (
-    <AuthContext.Provider value={user}>
+    <AuthContext.Provider value={authProvider}>
             {children}
     </AuthContext.Provider>
   )
